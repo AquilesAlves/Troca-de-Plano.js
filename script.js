edb18d5712e53bccdad8a0ac15cf062403f3b06b
Aquiles Alves <aquilespereira789@gmail.com>
2025-05-16 19:57:20 -0300

    u

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//calcula quando aperta o botão
 function calculaProporcional() {
     let valorAntigo = parseFloat(document.getElementById('valorAntigo').value)
     let valorNovo = parseFloat(document.getElementById('valorNovo').value)
@@ -6,15 +7,17 @@ function calculaProporcional() {
     const msg1 = (document.getElementById('msg1'))
     const msg2 = document.getElementById('msg2')
 
+    //valor diario de cada plano
     let diariaAntigo = valorAntigo/30
     let diariaNovo = valorNovo/30
 
+    //declaração de variaveis
     let diasPlanoAntigo = 0
     let diasPlanoNovo = 0
     let proporcionalAntigo = 0
     let proporcionalNovo = 0
 
-
+    //if else para fazer o calculo
     if (dataTroca > vencimento) {
         diasPlanoAntigo = dataTroca - vencimento
         diasPlanoNovo = vencimento - dataTroca + 30
@@ -31,38 +34,34 @@ function calculaProporcional() {
     }
     let valorTotal = proporcionalAntigo + proporcionalNovo
 
-
+    //formatar a casa decimal e a virgula
     valorAntigo = (valorAntigo).toFixed(2).replace('.', ',')
     valorNovo = (valorNovo).toFixed(2).replace('.', ',')
     valorTotal = (valorTotal).toFixed(2).replace('.', ',')
     proporcionalAntigo = (proporcionalAntigo).toFixed(2).replace('.', ',')
     proporcionalNovo = (proporcionalNovo).toFixed(2).replace('.', ',')
-
-    // msg1.innerHTML = `
-    // <p><strong>Valor do plano antigo:</strong> R$${(proporcionalAntigo).toFixed(2).replace('.', ',')}</p>
-    // <p><strong>Valor do plano novo:</strong> R$${(proporcionalNovo).toFixed(2).replace('.', ',')}</p>
-    // <p><strong>Proporcional total da troca:</strong> R$${(valorTotal).toFixed(2).replace('.', ',')}</p>
-    // `
     
+    //configurar o tamanho da região
     msg1.style.borderRadius = "15px"
     msg1.style.padding = "15px"
     msg1.style.height = "100px"
     msg1.style.width = "350px"
-
     msg1.innerHTML = `
         <p>Calculando o proporcional da troca de plano ficará o valor de R$${valorTotal} para o dia ${vencimento}, e as próximas faturas passarão para o novo valor do seu plano que é R$${valorNovo}. Posso seguir com troca de plano?</p>
     `
-    
+    document.getElementById('copy1').style.display = 'block'     //faz aparecer o botao
+
 
     msg2.style.borderRadius = "15px"
     msg2.style.padding = "15px"
     msg2.style.height = "100px"
     msg2.style.width = "350px"
-
     msg2.innerHTML = `
         Cliente solicitou a troca do plano
         Proporcional de ${diasPlanoAntigo} dias do plano antigo ${proporcionalAntigo}
         e ${diasPlanoNovo} dias do plano novo R$${proporcionalNovo}
         Ciente do novo valor R$${valorNovo}
-    `
+        `
+        document.getElementById('copy2').style.display = 'block'
+
 }
